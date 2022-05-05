3449deb33c6ceac64a27b70de26b04c48fbc650a
Kalle Chen <kallechen.tw@gmail.com>
2022-05-05 21:10:33 +0800

    Add tweet script

---
+++ scripts/tweet.mjs
@@ -0,0 +1,46 @@
+import 'zx/globals'
+import 'dotenv/config'
+import { TwitterApi } from 'twitter-api-v2'
+const { formatInTimeZone } = require('date-fns-tz')
+const { add } = require('date-fns')
+
+const data = require('../data/duelData.json')
+
+const TWITTER_API_KEY = process.env.TWITTER_API_KEY
+const TWITTER_API_SECRET = process.env.TWITTER_API_SECRET
+const TWITTER_ACCESS_TOKEN = process.env.TWITTER_ACCESS_TOKEN
+const TWITTER_ACCESS_TOKEN_SECRET = process.env.TWITTER_ACCESS_TOKEN_SECRET
+
+const getMainText = () => {
+  const time = `${formatInTimeZone(
+    add(new Date(), { weeks: -1 }),
+    'America/New_York',
+    'MMMM dd, yyyy'
+  )} ~ ${formatInTimeZone(new Date(), 'America/New_York', 'MMMM dd, yyyy')}`
+  let text = `🔥 Meta Decks on Tournament last Week\n📅 ${time}\n\n`
+  data.statistics
+    .filter((d) => d.count > 1)
+    .slice(0, 10)
+    .forEach((stat) => {
+      text += `🃏 ${stat.name.replace('@', '@ ')}: ${stat.count}\n`
+    })
+  text += '...\n\n#MasterDuel #Yugioh'
+  return text
+}
+
+try {
+  const client = new TwitterApi({
+    appKey: TWITTER_API_KEY,
+    appSecret: TWITTER_API_SECRET,
+    accessToken: TWITTER_ACCESS_TOKEN,
+    accessSecret: TWITTER_ACCESS_TOKEN_SECRET,
+  })
+  const mediaVideoId = await client.v1.uploadMedia(
+    path.resolve(__dirname, '../out/video.mp4')
+  )
+  const createdTweet = await client.v1.tweet(getMainText(), {
+    media_ids: mediaVideoId,
+  })
+} catch (e) {
+  console.log(e)
+}
